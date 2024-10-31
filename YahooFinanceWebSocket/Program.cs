ed9e0f6c4b9763e8a6031aad99d102b7ab50c72c
DavidS <dshe@users.noreply.github.com>
2024-11-01 05:29:56 +0800

    add observable

--- YahooFinanceWebSocket/Program.cs
+++ YahooFinanceWebSocket/Program.cs
@@ -1,45 +1,68 @@
 ﻿using ProtoBuf;
 using System;
 using System.Net.WebSockets;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using System.Text;
+namespace YahooFinanceWebSocket;
 
-namespace YahooFinanceWebSocket
+internal class Program
 {
-    internal class Program
+    static async Task Main()
     {
-        static async Task Main()
-        {
-            using ClientWebSocket client = new();
-            using CancellationTokenSource cts = new();
-            cts.CancelAfter(TimeSpan.FromSeconds(1200));
+        IObservable<PricingData> obs = CreateYahooFinanceObservable(["SPY", "EURUSD=X"]);
+        IDisposable d = obs.Subscribe(x => Console.WriteLine($"{x.Id}: {x.Price}, at {DateTimeOffset.FromUnixTimeMilliseconds(x.Time).TimeOfDay}"));
+        await Task.Delay(10000);
+        d.Dispose();
+        Console.Write("disposed");
+        await Task.Delay(5000);
+    }
 
+    public static IObservable<PricingData> CreateYahooFinanceObservable(IEnumerable<string> symbols)
+    {
+        byte[] requestMessage = CreateRequestMessage(symbols);
+        byte[] responseBuffer = new byte[1024];
+
+        return Observable.Create<PricingData>(async (observer, ct) =>
+        {
+            ClientWebSocket client = new();
+            await client.ConnectAsync(new Uri("wss://streamer.finance.yahoo.com/"), ct);
+            if (client.State != WebSocketState.Open)
+                observer.OnError(new Exception("Connection failed."));
+
+            await client.SendAsync(requestMessage, WebSocketMessageType.Text, true, ct);
             try
             {
-                await client.ConnectAsync(new Uri("wss://streamer.finance.yahoo.com/"), cts.Token);
-
-                if (client.State == WebSocketState.Open)
+                while (true)
                 {
-                    Console.WriteLine("Connection successful.");
-
-                    string message = """{"subscribe":["SPY", "INVE-B.ST", "^OMX", "SBB-B.ST"]}""";
-                    byte[] bytesToSend = Encoding.UTF8.GetBytes(message);
-                    await client.SendAsync(bytesToSend, WebSocketMessageType.Text, true, cts.Token);
-
-                    byte[] responseBuffer = new byte[1024];
-                    while (true)
-                    {
-                        WebSocketReceiveResult response = await client.ReceiveAsync(responseBuffer, cts.Token);
-                        string responseMessage = Encoding.UTF8.GetString(responseBuffer, 0, response.Count);
-                        //Console.WriteLine($"Message: {responseMessage}");
-
-                        byte[] bytes = Convert.FromBase64String(responseMessage);
-                        PricingData y = Serializer.Deserialize<PricingData>((ReadOnlySpan<byte>)bytes);
-
-                        Console.WriteLine($"{y.Id}: {y.Price}, at {DateTimeOffset.FromUnixTimeMilliseconds(y.Time).TimeOfDay}");
-                    }
+                    WebSocketReceiveResult response = await client.ReceiveAsync(responseBuffer, ct);
+                    string responseMessage = Encoding.UTF8.GetString(responseBuffer, 0, response.Count);
+                    byte[] bytes = Convert.FromBase64String(responseMessage);
+                    PricingData y = Serializer.Deserialize<PricingData>((ReadOnlySpan<byte>)bytes);
+                    observer.OnNext(y);
                 }
             }
-            catch (WebSocketException e) { Console.WriteLine(e.Message); }
-        }
+            catch (Exception e) 
+            { 
+                observer.OnError(e); 
+            }
+
+            return Disposable.Create(() =>
+            {
+                client.Dispose();
+            });
+
+        }).Publish().RefCount();
+    }
+
+    private static byte[] CreateRequestMessage(IEnumerable<string> symbols)
+    {
+        StringBuilder sb = new();
+        sb.Append("{\"subscribe\":[");
+        IEnumerable<string> symbolsList = symbols.Select(symbol => $"\"{symbol}\"");
+        sb.AppendJoin(", ", symbolsList);
+        sb.Append("]}");
+        string message = sb.ToString();
+        return Encoding.UTF8.GetBytes(message);
     }
 }
